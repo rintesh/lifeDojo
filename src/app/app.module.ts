592076e55843f6e62ff4e744577931e21117e47e
Rintesh <rinteshray@gmail.com>
2021-06-24 23:30:31 +0530

    component as attribute(doubt) class and element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
